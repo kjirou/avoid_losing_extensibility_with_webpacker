ab1b1eb8de0b4cfd2946d0638c02b1b86a70e29e
kjirou <sorenariblog@gmail.com>
2020-12-18 02:54:58 +0900

    Call jQuery without affecting the global scope

--- app/javascript/src/pages/users/show.js
+++ app/javascript/src/pages/users/show.js
@@ -1,9 +1,14 @@
+import $ from 'jquery'
+
 import {add} from '../shared/calculator'
 
 const handleDOMContentLoaded = (context) => {
   console.log('users/show here.')
   console.log(context.embeddedVariables.foo)
   console.log(add(1, 2))
+  $('.js-greeting-button').on('click', () => {
+    alert('Hi!')
+  })
 }
 
 export default {
