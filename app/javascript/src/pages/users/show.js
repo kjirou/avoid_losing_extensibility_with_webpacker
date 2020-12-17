80b9de9d19ac8aaafe1a7936694dde485f2d0f2c
kjirou <sorenariblog@gmail.com>
2020-12-18 02:22:44 +0900

    Define a module used in pages

--- app/javascript/src/pages/users/show.js
+++ app/javascript/src/pages/users/show.js
@@ -1,6 +1,9 @@
+import {add} from '../shared/calculator'
+
 const handleDOMContentLoaded = (context) => {
   console.log('users/show here.')
   console.log(context.embeddedVariables.foo)
+  console.log(add(1, 2))
 }
 
 export default {
